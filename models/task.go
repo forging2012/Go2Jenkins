615e21c573b540750eca8a91fda22f23b5027e48
YAO <yyf1986@users.noreply.github.com>
2017-06-10 13:41:42 +0800

    Create task.go

--- models/task.go
+++ models/task.go
@@ -6,11 +6,21 @@ import (
 	"time"
 	
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/toolbox"
 )
 
+func AddTask(name,spec string) {
+	f:= func() error { fmt.Println(name,time.Now()); return nil }
+    tk := toolbox.NewTask(name, spec, f)
+    toolbox.AddTask(name, tk)
+	tk.Run()
+	toolbox.StopTask()
+	toolbox.StartTask()
+	logs.Debug(tk.GetStatus())
+}
 
-func AddT(name,spec string) {
+func addTask(name,spec string) {
 	f:= func() error { fmt.Println(name,time.Now()); return nil }
     tk := toolbox.NewTask(name, spec, f)
     toolbox.AddTask(name, tk)
@@ -29,7 +39,7 @@ func LoadCron(){
     }
 	for _, b := range data {
             fmt.Println(b.Project, b.Spec)  //显示2组数据
-            AddT(b.Project,b.Spec)
+            addTask(b.Project,b.Spec)
     }
 }
 
