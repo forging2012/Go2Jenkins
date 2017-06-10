680059c02ae28465acd26e210af19bf341c56619
YAO <yyf1986@users.noreply.github.com>
2017-06-10 23:27:16 +0800

    Create task.go

--- models/task.go
+++ models/task.go
@@ -11,22 +11,44 @@ import (
 
 func AddTask(name,spec string) {
 	f:= func() error { fmt.Println(name,time.Now()); return nil }
-    	tk := toolbox.NewTask(name, spec, f)
-    	toolbox.AddTask(name, tk)
-	toolbox.StopTask()
-	toolbox.StartTask()
+	tk := toolbox.NewTask(name, spec, f)
+	toolbox.AddTask(name, tk)
+	time.Sleep(time.Second * 10)
+	//toolbox.StopTask()
+	//toolbox.StartTask()
 }
 
 func DelTask(name string) {
-    	toolbox.DeleteTask(name)
+    toolbox.DeleteTask(name)
 }
 
 func addTask(ci CronInfo) {
 	f:= func() error { fmt.Println(ci.Project,time.Now()); return nil }
-    	tk := toolbox.NewTask(ci.Project, ci.Spec, f)
-    	toolbox.AddTask(ci.Project, tk)
+	tk := toolbox.NewTask(ci.Project, ci.Spec, f)
+	toolbox.AddTask(ci.Project, tk)
+	//fmt.Println(toolbox.AdminTaskList)
+}
+func addTask4Monitor(){
+	f:= func() error { monitor(); return nil }
+	tk := toolbox.NewTask("monitor", "*/10 * * * * *", f)
+	toolbox.AddTask("monitor", tk)
 }
 
+func monitor(){
+	var tasklist map[string]toolbox.Tasker
+	tasklist = toolbox.AdminTaskList
+	var croninfos []*CronInfo
+	for taskname,tasker := range tasklist{
+		ci := &CronInfo{taskname, tasker.GetSpec()}
+		croninfos = append(croninfos,ci)
+	}
+	//切片序列化为json
+	if cronconfig, err := json.Marshal(&croninfos); err != nil {
+		panic(err)
+	} else {
+		fmt.Println(string(cronconfig))
+	}
+}
 
 type CronInfo struct {
 	Project string `json:"project"`
@@ -35,15 +57,18 @@ type CronInfo struct {
 
 func LoadCron(){
 	cronconig := beego.AppConfig.String("cron")
-	var data []CronInfo
-	if err := json.Unmarshal([]byte(cronconig), &data); err != nil {
+	var croninfos []CronInfo
+	//json反序列化可以存储struct的切片中
+	if err := json.Unmarshal([]byte(cronconig), &croninfos); err != nil {
             fmt.Println(err)
-    	}
-	for _, b := range data {
-			addTask(b)
-    	}
+    }
+	for _, croninfo := range croninfos {
+			addTask(croninfo)
+    }
 }
 
 func init(){
 	LoadCron()
+	addTask4Monitor()
+	//fmt.Println(toolbox.AdminTaskList)
 }
