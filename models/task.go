576d5b556d2975c3c631206a6886ae766b3b8214
YAO <yyf1986@users.noreply.github.com>
2017-06-11 23:17:33 +0800

    Create task.go

--- models/task.go
+++ models/task.go
@@ -13,7 +13,7 @@ func AddTask(name,spec string) {
 	f:= func() error { fmt.Println(name,time.Now()); return nil }
 	tk := toolbox.NewTask(name, spec, f)
 	toolbox.AddTask(name, tk)
-	time.Sleep(time.Second * 10)
+	//time.Sleep(time.Second * 10)
 	//toolbox.StopTask()
 	//toolbox.StartTask()
 }
@@ -22,12 +22,26 @@ func DelTask(name string) {
 	toolbox.DeleteTask(name)
 }
 
+func DelayTask(name string) {
+	tasklist := toolbox.AdminTaskList
+	for taskname,tasker := range tasklist{
+		if (taskname == "test1") {
+			next := tasker.GetNext()
+			fmt.Println("before",tasker.GetNext())
+			mm, _ := time.ParseDuration("10m")  
+			mm1 := next.Add(mm)
+			tasker.SetNext(mm1)
+			fmt.Println("after",tasker.GetNext())
+		}
+	}
+}
+//加载配置文件task
 func addTask(ci CronInfo) {
 	f:= func() error { fmt.Println(ci.Project,time.Now()); return nil }
 	tk := toolbox.NewTask(ci.Project, ci.Spec, f)
 	toolbox.AddTask(ci.Project, tk)
 }
-
+//监控所有task状态
 func addTask4Monitor(){
 	f:= func() error { monitor(); return nil }
 	tk := toolbox.NewTask("monitor", "*/10 * * * * *", f)
@@ -35,8 +49,8 @@ func addTask4Monitor(){
 }
 
 func monitor(){
-	var tasklist map[string]toolbox.Tasker
-	tasklist = toolbox.AdminTaskList
+	//var tasklist map[string]toolbox.Tasker
+	tasklist := toolbox.AdminTaskList
 	var croninfos []*CronInfo
 	for taskname,tasker := range tasklist{
 		ci := &CronInfo{taskname, tasker.GetSpec()}
@@ -55,7 +69,7 @@ type CronInfo struct {
 	Spec string `json:"spec"`
 }
 
-func LoadCron(){
+func loadCron4Config(){
 	cronconig := beego.AppConfig.String("cron")
 	var croninfos []CronInfo
 	//json反序列化可以存储struct的切片中
@@ -68,6 +82,6 @@ func LoadCron(){
 }
 
 func init(){
-	LoadCron()
+	loadCron4Config()
 	addTask4Monitor()
 }
