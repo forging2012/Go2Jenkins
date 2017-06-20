a878d98d92b4fa93ba0b7d77ec4e890237039d89
yaoyf <yaoyf@asiainfo.com>
2017-06-20 16:45:59 +0800

    修改返回说明

--- controllers/task.go
+++ controllers/task.go
@@ -47,7 +47,7 @@ func (t *TaskController) Del() {
 
 // @Title Get all Task
 // @Description Get all task
-// @Success 200 {object} models.CronAllInfo
+// @Success 200 {object} models.CronInfo
 // @Failure 403 body is empty
 // @router /getall [get]
 func (t *TaskController) GetALL() {
